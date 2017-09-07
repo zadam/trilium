16485576c84e463851bc85b78e4be72f086ffa8c
azivner <adam.zivner@gmail.com>
2017-09-06 21:41:12 -0400

    bootstrap needs to be included before jQuery UI, otherwise close icon in the dialog will be missing

--- static/js/init.js
+++ static/js/init.js
@@ -4,8 +4,6 @@ $(function() {
 
         if (fancyTree.length) {
             fancyTree.height($(window).height() - fancyTree.offset().top - 10);
-
-            console.log("height: ", $(window).height() - fancyTree.offset().top - 10);
         }
 
         const noteEditable = $('div.note-editable');
