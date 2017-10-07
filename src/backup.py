b7a5dca8038f3e253011dec1df37608e5253955b
azivner <adam.zivner@gmail.com>
2017-10-06 20:39:00 -0400

    cleanup of old backup files

--- src/backup.py
+++ src/backup.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
 import utils
-from sql import getOption, setOption
+from sql import getOption, setOption, commit
 import config_provider
 from shutil import copyfile
+import os
+import re
 
 def backup():
     now = utils.nowTimestamp()
@@ -20,3 +22,25 @@ def backup():
         copyfile(document_path, backup_directory + "/" + "backup-" + date_str + ".db")
 
         setOption('last_backup_date', now)
+        commit()
+
+        cleanup_old_backups()
+
+
+def cleanup_old_backups():
+    now = datetime.utcnow()
+    config = config_provider.getConfig()
+    backup_directory = config['Backup']['backupDirectory']
+
+    for file in os.listdir(backup_directory):
+        match = re.search(r"backup-([0-9 -:]+)\.db", file)
+
+        if match:
+            date_str = match.group(1)
+
+            date = datetime.strptime(date_str, "%Y-%m-%d %H:%M")
+
+            if (now - date).days > 30:
+                print("Removing old backup - " + file)
+
+                os.remove(backup_directory + "/" + file)
