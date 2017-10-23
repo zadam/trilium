f49963dc6774871bd00a5f90d566917521605677
azivner <adam.zivner@gmail.com>
2017-10-22 20:22:59 -0400

    remove old python backend files

--- src/config_provider.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-def get_config():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-
-    return config
--- src/utils.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-import time
-
-
-def now_timestamp():
-    return int(time.mktime(datetime.utcnow().timetuple()))
