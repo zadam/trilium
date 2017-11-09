cd5f742a7befa78c31558af48324856c5370bc10
azivner <adam.zivner@gmail.com>
2017-11-08 20:48:48 -0500

    fixes in build script

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+echo "Deleting dist"
+
 rm -r dist/*
+
+cp -r ../trilium-node-binaries/sqlite/* node_modules/sqlite3/lib/binding/
+
+cp -r ../trilium-node-binaries/scrypt/* node_modules/scrypt/bin/
 
 ./node_modules/.bin/electron-rebuild
 
 ./node_modules/.bin/electron-packager . --out=dist --platform=linux,win32 --overwrite
 
-cp -r ../trilium-node-bindings/* node_modules/sqlite3/lib/binding/
+# can't copy this before the packaging because the same file name is used for both linux and windows build
+cp ../trilium-node-binaries/scrypt.node ./dist/trilium-win32-x64/resources/app/node_modules/scrypt/build/Release/
+
+echo "Packaging windows distribution..."
+
+# possibly use zip: zip -r myfiles.zip mydir
 
 tar cfJ dist/win.tar.xz dist/trilium-win32-x64
