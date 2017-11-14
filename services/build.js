82614c1c73777f39237efd0e07c3d5613c56d5b5
azivner <adam.zivner@gmail.com>
2017-11-13 23:41:44 -0500

    fix build script

--- services/build.js
+++ services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2017-11-13T23:17:28-05:00", buildRevision: "f9056c6754293da6dc2803eb7f04e0902f08be6f" };
+module.exports = { buildDate:"2017-11-13T23:36:03-05:00", buildRevision: "fbce84e779951056c0e8b4d5395ee2fbe426555e" };
