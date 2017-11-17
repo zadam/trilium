a6bf04f8d4dc92445e6b4decd7dbbaa2f3e3ebaa
azivner <adam.zivner@gmail.com>
2017-11-16 23:55:50 -0500

    support for https

--- generate-cert.sh
+++ generate-cert.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-openssl genrsa -des3 -out cert.key 2048
-
-openssl req -new -key cert.key -out cert.csr
-
-# Remove passphrase from key
-cp cert.key cert.key.org
-
-openssl rsa -in cert.key.org -out cert.key
-
-# Generate self signed certificate
-openssl x509 -req -days 730 -in cert.csr -signkey cert.key -out cert.crt
-
-rm cert.key.org
-rm cert.csr
+openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 2000 -nodes
