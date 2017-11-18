6b226a319c6c92e3f975dc7c0881d52095e51284
azivner <adam.zivner@gmail.com>
2017-11-18 12:48:54 -0500

    cleaned up old CTR encryption methods

--- services/data_encryption.js
+++ services/data_encryption.js
@@ -1,12 +1,6 @@
 "use strict";
 
-const utils = require('./utils');
-const aesjs = require('./aes');
 const crypto = require('crypto');
-
-function getDataAes(dataKey) {
-    return new aesjs.ModeOfOperation.ctr(dataKey, new aesjs.Counter(5));
-}
 
 function arraysIdentical(a, b) {
     let i = a.length;
@@ -17,55 +11,9 @@ function arraysIdentical(a, b) {
     return true;
 }
 
-function decrypt(dataKey, encryptedBase64) {
-    if (!dataKey) {
-        return "[protected]";
-    }
-
-    const aes = getDataAes(dataKey);
-
-    const encryptedBytes = utils.fromBase64(encryptedBase64);
-
-    const decryptedBytes = aes.decrypt(encryptedBytes);
-
-    const digest = decryptedBytes.slice(0, 4);
-    const payload = decryptedBytes.slice(4);
-
-    const hashArray = sha256Array(payload);
-
-    const computedDigest = hashArray.slice(0, 4);
-
-    if (!arraysIdentical(digest, computedDigest)) {
-        return false;
-    }
-
-    return aesjs.utils.utf8.fromBytes(payload);
-}
-
-function encrypt(dataKey, plainText) {
-    if (!dataKey) {
-        throw new Error("No data key!");
-    }
-
-    const aes = getDataAes(dataKey);
-
-    const payload = Array.from(aesjs.utils.utf8.toBytes(plainText));
-    const digest = sha256Array(payload).slice(0, 4);
-
-    const digestWithPayload = digest.concat(payload);
-
-    const encryptedBytes = aes.encrypt(digestWithPayload);
-
-    return utils.toBase64(encryptedBytes);
-}
-
 function shaArray(content) {
     // we use this as simple checksum and don't rely on its security so SHA-1 is good enough
     return crypto.createHash('sha1').update(content).digest();
-}
-
-function sha256Array(content) {
-    return crypto.createHash('sha256').update(content).digest();
 }
 
 function pad(data) {
@@ -136,8 +84,6 @@ function noteTextIv(iv) {
 }
 
 module.exports = {
-    decrypt,
-    encrypt,
     encryptCbc,
     decryptCbc,
     decryptCbcString,
--- services/password_encryption.js
+++ services/password_encryption.js
@@ -1,8 +1,6 @@
 const options = require('./options');
 const my_scrypt = require('./my_scrypt');
 const utils = require('./utils');
-const crypto = require('crypto');
-const aesjs = require('./aes');
 const data_encryption = require('./data_encryption');
 
 async function verifyPassword(password) {
@@ -11,31 +9,6 @@ async function verifyPassword(password) {
     const dbPasswordHash = await options.getOption('password_verification_hash');
 
     return givenPasswordHash === dbPasswordHash;
-}
-
-function decryptDataKey(passwordDerivedKey, encryptedBase64) {
-    const encryptedBytes = utils.fromBase64(encryptedBase64);
-
-    const aes = getAes(passwordDerivedKey);
-    return Array.from(aes.decrypt(encryptedBytes).slice(4));
-}
-
-function encryptDataKey(passwordDerivedKey, plainText) {
-    const aes = getAes(passwordDerivedKey);
-
-    const plainTextBuffer = Buffer.from(plainText, 'latin1');
-
-    const digest = crypto.createHash('sha256').update(plainTextBuffer).digest().slice(0, 4);
-
-    const encryptedBytes = aes.encrypt(Buffer.concat([digest, plainTextBuffer]));
-
-    return utils.toBase64(encryptedBytes);
-}
-
-async function setDataKey(passwordDerivedKey, plainText) {
-    const newEncryptedDataKey = encryptDataKey(passwordDerivedKey, plainText);
-
-    await options.setOption('encrypted_data_key', newEncryptedDataKey);
 }
 
 async function setDataKeyCbc(password, plainText) {
@@ -52,16 +25,6 @@ async function setDataKeyCbc(password, plainText) {
     await options.setOption('encrypted_data_key', newEncryptedDataKey);
 }
 
-async function getDecryptedDataKey(password) {
-    const passwordDerivedKey = await my_scrypt.getPasswordDerivedKey(password);
-
-    const encryptedDataKey = await options.getOption('encrypted_data_key');
-
-    const decryptedDataKey = decryptDataKey(passwordDerivedKey, encryptedDataKey);
-
-    return decryptedDataKey;
-}
-
 async function getDecryptedDataKeyCbc(password) {
     const passwordDerivedKey = await my_scrypt.getPasswordDerivedKey(password);
 
@@ -73,14 +36,8 @@ async function getDecryptedDataKeyCbc(password) {
     return decryptedDataKey;
 }
 
-function getAes(key) {
-    return new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-}
-
 module.exports = {
     verifyPassword,
-    getDecryptedDataKey,
     getDecryptedDataKeyCbc,
-    setDataKey,
     setDataKeyCbc
 };
