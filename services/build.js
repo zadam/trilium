17c90262c27b2da08784971f75c5792e24632852
azivner <adam.zivner@gmail.com>
2017-12-13 20:34:21 -0500

    create sync records only if sync is configured. This will prevent sync conflicts after document fork.

--- services/build.js
+++ services/build.js
@@ -1,1 +1,1 @@
-module.exports = { build_date:"2017-12-10T23:23:04-05:00", build_revision: "bdeaa2829d4c5436e86d0d943a2a332d0c016804" };
+module.exports = { build_date:"2017-12-12T23:57:58-05:00", build_revision: "2fa57b79fd972b3eda6eb8ef1d26e9915b75fd96" };
