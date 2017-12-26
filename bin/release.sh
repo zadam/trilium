7e1cc729f9959ba8ecfd0762e19af64b7d3b8b95
azivner <adam.zivner@gmail.com>
2017-12-25 21:06:35 -0500

    have to add changed files to index before committing

--- bin/release.sh
+++ bin/release.sh
@@ -22,7 +22,11 @@ echo "Releasing Trilium $VERSION"
 
 jq '.version = "'$VERSION'"' package.json|sponge package.json
 
+git add package.json
+
 echo 'module.exports = { build_date:"'`date --iso-8601=seconds`'", build_revision: "'`git log -1 --format="%H"`'" };' > services/build.js
+
+git add services/build.js
 
 TAG=v$VERSION
 
