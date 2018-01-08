865d29863186156b1924376b789930a867a61755
azivner <adam.zivner@gmail.com>
2018-01-07 22:10:13 -0500

    release 0.3.1

--- services/build.js
+++ services/build.js
@@ -1,1 +1,1 @@
-module.exports = { build_date:"2018-01-07T21:56:39-05:00", build_revision: "29eb88bac3bbc1db17bb4ed75f9b8bfe16333967" };
+module.exports = { build_date:"2018-01-07T22:10:13-05:00", build_revision: "86b14109527ff9d3816916fbed07a7c9648547f0" };
