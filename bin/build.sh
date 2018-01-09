4e5a95a1ac958c76f2561e7eba65e2a0f9a93030
azivner <adam.zivner@gmail.com>
2018-01-08 20:40:20 -0500

    fix scrypt binary for windows

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,6 @@ echo "Deleting dist"
 rm -r dist/*
 
 cp -r bin/deps/sqlite/* node_modules/sqlite3/lib/binding/
-cp -r bin/deps/scrypt/* node_modules/scrypt/bin/
 cp -r bin/deps/image/cjpeg.exe node_modules/mozjpeg/vendor/
 cp -r bin/deps/image/pngquant.exe node_modules/pngquant-bin/vendor/
 cp -r bin/deps/image/gifsicle.exe node_modules/giflossy/vendor/
@@ -21,4 +20,4 @@ cp -r bin/deps/image/gifsicle.exe node_modules/giflossy/vendor/
 ./node_modules/.bin/electron-packager . --out=dist --platform=win32 --arch=x64 --overwrite
 
 # can't copy this before the packaging because the same file name is used for both linux and windows build
-cp bin/deps/scrypt/win32-x64-57/scrypt.node ./dist/trilium-win32-x64/resources/app/node_modules/scrypt/build/Release/
+cp bin/deps/scrypt.node ./dist/trilium-win32-x64/resources/app/node_modules/scrypt/build/Release/
