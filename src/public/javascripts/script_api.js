001a5107dde7840f21fd2603bf712fdd4cdc900d
azivner <adam.zivner@gmail.com>
2018-03-24 23:45:36 -0400

    ScriptApi separated from ScriptContext, preparation for modularisation

--- src/public/javascripts/script_api.js
+++ src/public/javascripts/script_api.js
@@ -1,25 +1,3 @@
-function ScriptContext(startNote, allNotes) {
-    const modules = {};
-
-    return {
-        modules: modules,
-        notes: utils.toObject(allNotes, note => [note.noteId, note]),
-        apis: utils.toObject(allNotes, note => [note.noteId, ScriptApi(startNote, note)]),
-        require: moduleNoteIds => {
-            return moduleName => {
-                const candidates = allNotes.filter(note => moduleNoteIds.includes(note.noteId));
-                const note = candidates.find(c => c.title === moduleName);
-
-                if (!note) {
-                    throw new Error("Could not find module note " + moduleName);
-                }
-
-                return modules[note.noteId].exports;
-            }
-        }
-    };
-}
-
 function ScriptApi(startNote, currentNote) {
     const $pluginButtons = $("#plugin-buttons");
 
