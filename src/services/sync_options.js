de4ace5ada29b17befca59eff82b2d999cdf50b8
azivner <adam.zivner@gmail.com>
2018-08-28 21:13:40 +0200

    overriding sync settings from config file, fixes #160

--- src/services/sync_options.js
+++ src/services/sync_options.js
@@ -1,10 +1,22 @@
 "use strict";
 
 const optionService = require('./options');
+const config = require('./config');
+
+/*
+ * Primary configuration for sync is in the options (document), but we allow to override
+ * these settings in config file. The reason for that is to avoid a mistake of loading a live/production
+ * document with live sync settings in a dev/debug environment. Changes would then successfully propagate
+ * to live sync server.
+ */
+
+async function get(name) {
+    return config['Sync'][name] || await optionService.getOption(name);
+}
 
 module.exports = {
-    getSyncServerHost: async () => await optionService.getOption('syncServerHost'),
-    isSyncSetup: async () => !!await optionService.getOption('syncServerHost'),
-    getSyncTimeout: async () => await optionService.getOption('syncServerTimeout'),
-    getSyncProxy: async () => await optionService.getOption('syncProxy')
+    getSyncServerHost: async () => await get('syncServerHost'),
+    isSyncSetup: async () => !!await get('syncServerHost'),
+    getSyncTimeout: async () => await get('syncServerTimeout'),
+    getSyncProxy: async () => await get('syncProxy')
 };
