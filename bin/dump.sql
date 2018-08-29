bc61f17a3aae0be79d990a496a28b781d3c70b7b
azivner <adam.zivner@gmail.com>
2018-08-29 21:36:42 +0200

    changes in initial document

--- bin/dump.sql
+++ bin/dump.sql
@@ -1,19 +1,19 @@
 .mode insert branches
 .out db/main_branches.sql
-select * from branches;
+select * from branches where isDeleted = 0;
 
 .mode insert notes
 .out db/main_notes.sql
-select * from notes;
+select * from notes where isDeleted = 0;
 
 .mode insert images
 .out db/main_images.sql
-select * from images;
+select * from images where isDeleted = 0;
 
 .mode insert note_images
 .out db/main_note_images.sql
-select * from note_images;
+select * from note_images where isDeleted = 0;
 
 .mode insert attributes
 .out db/main_attributes.sql
-select * from attributes;
+select * from attributes where isDeleted = 0;
