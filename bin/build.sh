492fde6f46067df86fd66d27d339693263105230
azivner <adam.zivner@gmail.com>
2018-09-06 23:23:19 +0200

    fix copy scrypt binary to correct directory

--- bin/build.sh
+++ bin/build.sh
@@ -29,7 +29,7 @@ cp -r bin/deps/sqlite/* $WIN_RES_DIR/node_modules/sqlite3/lib/binding/
 cp bin/deps/image/cjpeg.exe $WIN_RES_DIR/node_modules/mozjpeg/vendor/
 cp bin/deps/image/pngquant.exe $WIN_RES_DIR/node_modules/imagemin-pngquant/node_modules/pngquant-bin/vendor/
 cp bin/deps/image/gifsicle.exe $WIN_RES_DIR/node_modules/giflossy/vendor/
-cp bin/deps/scrypt.node $WIN_RES_DIR/node_modules/scrypt/build/Release/
+cp bin/deps/scrypt.node $WIN_RES_DIR/node_modules/@mlink/scrypt/build/Release/
 
 echo "Cleaning up unnecessary binaries from all builds"
 
