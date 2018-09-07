50cd0c792c48c668bf9f03e6a2110691849a2fa7
azivner <adam.zivner@gmail.com>
2018-09-07 13:47:26 +0200

    provide H4-H6, fixes #177

--- src/public/javascripts/services/note_detail_text.js
+++ src/public/javascripts/services/note_detail_text.js
@@ -18,16 +18,7 @@ async function show() {
         // textEditor might have been initialized during previous await so checking again
         // looks like double initialization can freeze CKEditor pretty badly
         if (!textEditor) {
-            textEditor = await BalloonEditor.create($noteDetailText[0], {
-                heading: {
-                    options: [
-                        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                        { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                        { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-                        { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' }
-                    ]
-                }
-            });
+            textEditor = await BalloonEditor.create($noteDetailText[0]);
 
             onNoteChange(noteDetailService.noteChanged);
         }
