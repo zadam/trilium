8cbb29ee251c432f437f3afe620e0a7d27ca0cc3
azivner <adam.zivner@gmail.com>
2018-11-30 19:56:20 +0100

    default location for data dir is now .local for linux and Roaming for windows

--- src/services/data_dir.js
+++ src/services/data_dir.js
@@ -1,13 +1,63 @@
 "use strict";
 
+/*
+ * This file resolves trilium data path in this order of priority:
+ * - if TRILIUM_DATA_DIR environment variable exists, then its value is used as the path
+ * - if "trilium-data" dir exists directly in the home dir, then it is used
+ * - based on OS convention, if the "app data directory" exists, we'll use or create "trilium-data" directory there
+ * - as a fallback if previous step fails, we'll use home dir
+ */
+
 const os = require('os');
 const fs = require('fs');
 
-const TRILIUM_DATA_DIR = process.env.TRILIUM_DATA_DIR || os.homedir() + "/trilium-data";
+function getAppDataDir() {
+    let appDataDir = os.homedir(); // fallback if OS is not recognized
 
-if (!fs.existsSync(TRILIUM_DATA_DIR)) {
-    fs.mkdirSync(TRILIUM_DATA_DIR, 0o700);
+    if (os.platform() === 'win32') {
+        appDataDir = process.env.APPDATA;
+    }
+    else if (os.platform() === 'linux') {
+        appDataDir = os.homedir() + '/.local/share';
+    }
+    else if (os.platform() === 'darwin') {
+        appDataDir = os.homedir() + '/Library/Application Support';
+    }
+
+    if (!fs.existsSync(appDataDir)) {
+        // expected app data path doesn't exist, let's use fallback
+        appDataDir = os.homedir();
+    }
+
+    return appDataDir;
 }
+
+const DIR_NAME = 'trilium-data';
+
+function getTriliumDataDir() {
+    if (process.env.TRILIUM_DATA_DIR) {
+        return process.env.TRILIUM_DATA_DIR;
+    }
+
+    const homePath = os.homedir() + "/" + DIR_NAME;
+
+    if (fs.existsSync(homePath)) {
+        return homePath;
+    }
+
+    const appDataPath = getAppDataDir() + '/' + DIR_NAME;
+
+    if (!fs.existsSync(appDataPath)) {
+        fs.mkdirSync(appDataPath, 0o700);
+    }
+
+    return appDataPath;
+}
+
+const TRILIUM_DATA_DIR =  getTriliumDataDir();
+
+// not necessary to log this since if we have logs we already know where data dir is.
+console.log("Using data dir:", TRILIUM_DATA_DIR);
 
 const DOCUMENT_PATH = TRILIUM_DATA_DIR + "/document.db";
 const BACKUP_DIR = TRILIUM_DATA_DIR + "/backup";
