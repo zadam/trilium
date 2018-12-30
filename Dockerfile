de9e0c79290d3dc547b78a8e66ba6e0339a4e72e
azivner <adam.zivner@gmail.com>
2018-12-30 21:13:35 +0100

    lazy loading icon for both mobile and desktop

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:10.14.0-alpine
+FROM node:10.15.0-alpine
 
 # Create app directory
 WORKDIR /usr/src/app
--- bin/build-server.sh
+++ bin/build-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PKG_DIR=dist/trilium-linux-x64-server
-NODE_VERSION=10.14.1
+NODE_VERSION=10.15.0
 
 rm -r $PKG_DIR
 mkdir $PKG_DIR
