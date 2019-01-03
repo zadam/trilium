e2dfe1b6de46b2a485447e155468b0f7f4e6dccb
azivner <adam.zivner@gmail.com>
2019-01-03 22:32:36 +0100

    fixed setup page issues which caused wrong choice to be used in the wizard

--- src/public/javascripts/setup.js
+++ src/public/javascripts/setup.js
@@ -25,26 +25,20 @@ function SetupModel() {
 
     this.instanceType = utils.isElectron() ? "desktop" : "server";
 
-    this.setupTypeSelected = this.getSetupType = () =>
-        this.setupNewDocument()
-        || this.setupSyncFromDesktop()
-        || this.setupSyncFromServer();
+    this.setupTypeSelected = () => !!this.setupType();
 
     this.selectSetupType = () => {
-        this.step(this.getSetupType());
-        this.setupType(this.getSetupType());
+        this.step(this.setupType());
     };
 
     this.back = () => {
         this.step("setup-type");
 
-        this.setupNewDocument(false);
-        this.setupSyncFromServer(false);
-        this.setupSyncFromDesktop(false);
+        this.setupType("");
     };
 
     this.finish = async () => {
-        if (this.setupNewDocument()) {
+        if (this.setupType() === 'new-document') {
             const username = this.username();
             const password1 = this.password1();
             const password2 = this.password2();
@@ -72,7 +66,7 @@ function SetupModel() {
                 window.location.replace("/");
             });
         }
-        else if (this.setupSyncFromServer()) {
+        else if (this.setupType() === 'sync-from-server') {
             const syncServerHost = this.syncServerHost();
             const syncProxy = this.syncProxy();
             const username = this.username();
