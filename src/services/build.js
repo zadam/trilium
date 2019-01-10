2b32addade6ee6883cc3468f4b803facf946c07d
azivner <adam.zivner@gmail.com>
2019-01-10 21:31:30 +0100

    release 0.27.4

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2019-01-05T22:48:11+01:00", buildRevision: "9fca7f09a564c719c834d38d76c3b595c8579b2a" };
+module.exports = { buildDate:"2019-01-10T21:31:30+01:00", buildRevision: "0b251530fa0ee61edc8dcc9235033abb73afc614" };
