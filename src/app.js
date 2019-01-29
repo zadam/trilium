5619088c414b01e6cb0a8735b315e7034eba123b
zadam <adam.zivner@gmail.com>
2019-01-29 21:19:08 +0100

    raise payload size limit to 500 MB #395

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
     });
 });
 
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({limit: '500mb'}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
