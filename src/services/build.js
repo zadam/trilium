6f956c241586117227929b9ca2b947f87ff5942e
zadam <adam.zivner@gmail.com>
2019-02-20 23:08:36 +0100

    release 0.30.1-beta

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2019-02-20T21:58:31+01:00", buildRevision: "ff67b8a0ba9c37827b3d3f1e68121bbf28c80393" };
+module.exports = { buildDate:"2019-02-20T23:08:36+01:00", buildRevision: "3533160bef78e00cf906a436ff7b366bdec271be" };
