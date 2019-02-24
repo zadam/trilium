ec384fafce336fc5b98c7fd6d12319684fdd1c79
jkurei <0xjkurei@gmail.com>
2019-02-24 01:34:23 +0100

    Querying by dateCreated and dateModified virtual attributes

--- src/services/parse_filters.js
+++ src/services/parse_filters.js
@@ -1,8 +1,30 @@
+const dayjs = require("dayjs");
+
+const labelRegex = /(\b(and|or)\s+)?@(!?)([\w_-]+|"[^"]+")((=|!=|<|<=|>|>=)([\w_-]+|"[^"]+"))?/i;
+const smartValueRegex = /^(TODAY|NOW)((\+|\-)(\d+)(H|D|M|Y)){0,1}$/i;
+
+function calculateSmartValue(v) {
+    const normalizedV = v.toUpperCase() + "+0D"; // defaults of sorts
+    const [ , keyword, sign, snum, unit] = /(TODAY|NOW)(\+|\-)(\d+)(H|D|M|Y)/.exec(normalizedV);
+    const num = parseInt(snum);
+
+    if (keyword != "TODAY" && keyword != "NOW") {
+        return v;
+    }
+
+    const fullUnit = {
+        TODAY: { D: "days", M: "months", Y: "years" },
+        NOW: { D: "days", M: "minutes", H: "hours" }
+    }[keyword][unit];
+
+    const format = keyword == "TODAY" ? "YYYY-MM-DD" : "YYYY-MM-DDTHH:mm";
+    const date = (sign == "+" ? dayjs().add(num, fullUnit) : dayjs().subtract(num, fullUnit));
+
+    return date.format(format);
+}
+
 module.exports = function(searchText) {
     const labelFilters = [];
-
-    const labelRegex = /(\b(and|or)\s+)?@(!?)([\w_-]+|"[^"]+")((=|!=|<|<=|>|>=)([\w_-]+|"[^"]+"))?/i;
-
     let match = labelRegex.exec(searchText);
 
     function trimQuotes(str) { return str.startsWith('"') ? str.substr(1, str.length - 2) : str; }
@@ -11,11 +33,17 @@ module.exports = function(searchText) {
         const relation = match[2] !== undefined ? match[2].toLowerCase() : 'and';
         const operator = match[3] === '!' ? 'not-exists' : 'exists';
 
+        const value = match[7] !== undefined ? trimQuotes(match[7]) : null
+
         labelFilters.push({
             relation: relation,
             name: trimQuotes(match[4]),
             operator: match[6] !== undefined ? match[6] : operator,
-            value: match[7] !== undefined ? trimQuotes(match[7]) : null
+            value: (
+                value && value.match(smartValueRegex)
+                    ? calculateSmartValue(value)
+                    : value
+            )
         });
 
         // remove labels from further fulltext search
@@ -24,5 +52,5 @@ module.exports = function(searchText) {
         match = labelRegex.exec(searchText);
     }
 
-    return {labelFilters: labelFilters, searchText};
+    return { labelFilters: labelFilters, searchText };
 };
