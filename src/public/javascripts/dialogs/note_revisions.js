f47f3461e666d007668e03282f86b028012f7bef
zadam <zadam.apps@gmail.com>
2019-04-01 21:49:27 +0200

    show local date in revisions dialog + make it larger

--- src/public/javascripts/dialogs/note_revisions.js
+++ src/public/javascripts/dialogs/note_revisions.js
@@ -26,11 +26,9 @@ async function showNoteRevisionsDialog(noteId, noteRevisionId) {
     revisionItems = await server.get('notes/' + noteId + '/revisions');
 
     for (const item of revisionItems) {
-        const utcDateModified = utils.parseDate(item.utcDateModifiedFrom);
-
         $list.append($('<option>', {
             value: item.noteRevisionId,
-            text: utils.formatDateTime(utcDateModified)
+            text: item.dateModifiedFrom
         }));
     }
 
