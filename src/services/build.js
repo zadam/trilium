70591eadd5eca7ccdc1ce3966c2c9fd8894a4cf0
zadam <zadam.apps@gmail.com>
2019-06-20 09:39:11 +0200

    release 0.33.2-beta

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2019-06-16T11:13:37+02:00", buildRevision: "93f1d461044c7e027a739fad0ebd2582a71edc34" };
+module.exports = { buildDate:"2019-06-20T09:39:11+02:00", buildRevision: "fead3cd7ade63620655376dc5e85b4308ec71bde" };
