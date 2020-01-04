b8f2797abfeb2faa62630cdfff2b376c8ed3ee9f
zadam <zadam.apps@gmail.com>
2020-01-04 09:04:08 +0100

    better behavior of note info widget in tight width

--- src/public/javascripts/widgets/note_info.js
+++ src/public/javascripts/widgets/note_info.js
@@ -1,18 +1,18 @@
 import StandardWidget from "./standard_widget.js";
 
 const TPL = `
-<table class="note-info-table">
+<table class="note-info-table" style="table-layout: fixed; width: 100%;">
     <tr>
-        <th>Note ID:</th>
-        <td colspan="3" class="note-info-note-id"></td>
+        <th nowrap>Note ID:</th>
+        <td nowrap colspan="3" class="note-info-note-id"></td>
     </tr>
     <tr>
-        <th>Created:</th>
-        <td colspan="3" class="note-info-date-created"></td>
+        <th nowrap>Created:</th>
+        <td nowrap colspan="3" style="overflow: hidden; text-overflow: ellipsis;" class="note-info-date-created"></td>
     </tr>
     <tr>
-        <th>Modified:</th>
-        <td colspan="3" class="note-info-date-modified"></td>
+        <th nowrap>Modified:</th>
+        <td nowrap colspan="3" style="overflow: hidden; text-overflow: ellipsis;" class="note-info-date-modified"></td>
     </tr>
     <tr>
         <th>Type:</th>
@@ -39,10 +39,19 @@ class NoteInfoWidget extends StandardWidget {
         const note = this.ctx.note;
 
         $noteId.text(note.noteId);
-        $dateCreated.text(note.dateCreated);
-        $dateModified.text(note.dateModified);
+        $dateCreated
+            .text(note.dateCreated)
+            .attr("title", note.dateCreated);
+
+        $dateModified
+            .text(note.dateModified)
+            .attr("title", note.dateCreated);
+
         $type.text(note.type);
-        $mime.text(note.mime).attr("title", note.mime);
+
+        $mime
+            .text(note.mime)
+            .attr("title", note.mime);
     }
 
     eventReceived(name, data) {
