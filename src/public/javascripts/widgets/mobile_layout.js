ed1b5e38438bfba49e156e5069df83f9c3be6b15
zadam <zadam.apps@gmail.com>
2020-03-01 12:05:10 +0100

    mobile layout fixes

--- src/public/javascripts/widgets/mobile_layout.js
+++ src/public/javascripts/widgets/mobile_layout.js
@@ -11,9 +11,11 @@ export default class MobileLayout {
             .id('root-widget')
             .css('height', '100vh')
             .child(new FlexContainer('column')
+                .class("d-sm-flex d-md-flex d-lg-flex d-xl-flex col-12 col-sm-5 col-md-4 col-lg-4 col-xl-4")
                 .child(new MobileGlobalButtonsWidget())
                 .child(new NoteTreeWidget()))
             .child(new FlexContainer('column')
+                .class("d-none d-sm-flex d-md-flex d-lg-flex d-xl-flex col-12 col-sm-7 col-md-8 col-lg-8")
                 .child(new NoteTitleWidget())
                 .child(new NoteDetailWidget()));
     }
