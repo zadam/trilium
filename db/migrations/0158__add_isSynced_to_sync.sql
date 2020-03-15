f5e27278ab2a38484019ee2510781099b60ec2b6
zadam <zadam.apps@gmail.com>
2020-03-15 11:19:30 +0100

    fix migration script to preserve sync IDs

--- db/migrations/0158__add_isSynced_to_sync.sql
+++ db/migrations/0158__add_isSynced_to_sync.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS "sync_mig" (
-    `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `entityName`	TEXT NOT NULL,
-    `entityId`	TEXT NOT NULL,
-    `sourceId` TEXT NOT NULL,
-    `isSynced` INTEGER default 0 not null,
-    `utcSyncDate`	TEXT NOT NULL);
+                                          `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                          `entityName`	TEXT NOT NULL,
+                                          `entityId`	TEXT NOT NULL,
+                                          `sourceId` TEXT NOT NULL,
+                                          `isSynced` INTEGER default 0 not null,
+                                          `utcSyncDate`	TEXT NOT NULL);
 
-INSERT INTO sync_mig (entityName, entityId, sourceId, isSynced, utcSyncDate)
-SELECT entityName, entityId, sourceId, 1, utcSyncDate FROM sync;
+INSERT INTO sync_mig (id, entityName, entityId, sourceId, isSynced, utcSyncDate)
+SELECT id, entityName, entityId, sourceId, 1, utcSyncDate FROM sync;
 
 DROP TABLE sync;
 
