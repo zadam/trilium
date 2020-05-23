cb4d0624b53ed163c29c3857ca1836e29ab8a59d
zadam <adam.zivner@gmail.com>
2020-05-24 00:21:20 +0200

    put session directory into data dir to avoid conflict of multiple instances on a single server, fixes #1033

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const FileStore = require('session-file-store')(session);
 const os = require('os');
 const sessionSecret = require('./services/session_secret');
 const cls = require('./services/cls');
+const dataDir = require('./services/data_dir');
 require('./entities/entity_constructor');
 require('./services/handlers');
 require('./services/hoisted_note_loader');
@@ -58,7 +59,7 @@ const sessionParser = session({
     },
     store: new FileStore({
         ttl: 30 * 24 * 3600,
-        path: os.tmpdir() + '/trilium-sessions'
+        path: dataDir.TRILIUM_DATA_DIR + '/sessions'
     })
 });
 app.use(sessionParser);
