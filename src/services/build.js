38723e0189c5f24ef54163d138717c4240fac208
zadam <adam.zivner@gmail.com>
2020-05-31 23:33:30 +0200

    release 0.42.5

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2020-05-31T10:33:12+02:00", buildRevision: "50a28d8c5198606ee5d92696095c1c97397592e8" };
+module.exports = { buildDate:"2020-05-31T23:33:30+02:00", buildRevision: "8c88ce6f651c3df1cd97925e5b8e3281ca4b1665" };
