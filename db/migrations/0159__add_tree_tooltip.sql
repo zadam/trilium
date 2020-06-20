bfc3e7dc26c98259193b5994a31723e5a46e2648
Shon Ramamurthy <shon.rp@protonmail.com>
2020-06-20 16:04:04 +0530

    Revert DB Upgrade
    The db upgrade is reverted as this is not required for options.

--- db/migrations/0159__add_tree_tooltip.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO options (name, value, utcDateCreated, utcDateModified, isSynced)
-VALUES ('treeNoteTitleTooltipEnabled', 'false', '2020-06-14T16:24:00.874Z', '2020-06-14T16:24:00.874Z', 0);
