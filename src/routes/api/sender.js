fd8b678a0db0e746e9110e391acedaee83b2118b
Casey Doran <caseyd.doran@gmail.com>
2020-10-15 23:25:56 -0700

    if the sender request contains a label and/or a labelValue, apply it

--- src/routes/api/sender.js
+++ src/routes/api/sender.js
@@ -35,6 +35,14 @@ function saveNote(req) {
         mime: 'text/html'
     });
 
+    if (req.body.label && req.body.label.trim()){
+        let value;
+        if (req.body.labelValue && req.body.labelValue.trim()){
+            value = req.body.labelValue;
+        }
+        note.setLabel(req.body.label, value);
+    }
+
     return {
         noteId: note.noteId,
         branchId: branch.branchId
