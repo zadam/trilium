01a82bed093f5b9427c0e046e6c3219129c82cce
zadam <adam.zivner@gmail.com>
2021-01-29 23:36:33 +0100

    extra window layout updated to be consistent with main window

--- src/public/app/layouts/desktop_extra_window_layout.js
+++ src/public/app/layouts/desktop_extra_window_layout.js
@@ -9,6 +9,14 @@ import NoteTypeWidget from "../widgets/note_type.js";
 import NoteActionsWidget from "../widgets/note_actions.js";
 import NoteDetailWidget from "../widgets/note_detail.js";
 import OwnedAttributeListWidget from "../widgets/attribute_widgets/owned_attribute_list.js";
+import CollapsibleSectionContainer from "../widgets/collapsible_section_container.js";
+import SearchDefinitionWidget from "../widgets/search_definition.js";
+import PromotedAttributesWidget from "../widgets/attribute_widgets/promoted_attributes.js";
+import InheritedAttributesWidget from "../widgets/inherited_attribute_list.js";
+import Container from "../widgets/container.js";
+import SqlTableSchemasWidget from "../widgets/sql_table_schemas.js";
+import NoteListWidget from "../widgets/note_list.js";
+import SqlResultWidget from "../widgets/sql_result.js";
 
 export default class DesktopExtraWindowLayout {
     constructor(customWidgets) {
@@ -37,8 +45,21 @@ export default class DesktopExtraWindowLayout {
                         .child(new NoteTypeWidget().hideInZenMode())
                         .child(new NoteActionsWidget().hideInZenMode())
                     )
-                    .child(new TabCachingWidget(() => new OwnedAttributeListWidget()))
-                    .child(new TabCachingWidget(() => new NoteDetailWidget()))
+                    .child(
+                        new TabCachingWidget(() => new CollapsibleSectionContainer()
+                            .child(new SearchDefinitionWidget())
+                            .child(new PromotedAttributesWidget())
+                            .child(new OwnedAttributeListWidget())
+                            .child(new InheritedAttributesWidget())
+                        )
+                    )
+                    .child(new Container()
+                        .css('height: 100%; overflow: auto;')
+                        .child(new TabCachingWidget(() => new SqlTableSchemasWidget()))
+                        .child(new TabCachingWidget(() => new NoteDetailWidget()))
+                        .child(new TabCachingWidget(() => new NoteListWidget()))
+                        .child(new TabCachingWidget(() => new SqlResultWidget()))
+                    )
                     .child(...this.customWidgets.get('center-pane'))
                 )
             );
