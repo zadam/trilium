ba33a0d330dc732f0f2e33361f9c1ee721ce69b1
zadam <adam.zivner@gmail.com>
2021-01-29 20:27:16 +0100

    fix conflict between CKEditor build content style and externally provided content style (which are not needed when build CSS is available), #1590

--- src/public/app/widgets/type_widgets/read_only_text.js
+++ src/public/app/widgets/type_widgets/read_only_text.js
@@ -67,6 +67,11 @@ export default class ReadOnlyTextTypeWidget extends AbstractTextTypeWidget {
     }
 
     async doRefresh(note) {
+        // we load CKEditor also for read only notes because they contain content styles required for correct rendering of even read only notes
+        // we could load just ckeditor-content.css but that causes CSS conflicts when both build CSS and this content CSS is loaded at the same time
+        // (see https://github.com/zadam/trilium/issues/1590 for example of such conflict)
+        await libraryLoader.requireLibrary(libraryLoader.CKEDITOR);
+
         const noteComplement = await treeCache.getNoteComplement(note.noteId);
 
         this.$content.html(noteComplement.content);
