44aa2834b3dafc3af796407fb5f6fa6a1b53e8a5
zadam <adam.zivner@gmail.com>
2021-04-11 22:29:56 +0200

    release 0.47.0-beta

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2021-04-03T22:37:04+02:00", buildRevision: "ef37a52a06b471e60f9c0f11da704283bbcef6ab" };
+module.exports = { buildDate:"2021-04-11T22:29:56+02:00", buildRevision: "58e4bd4974275a113c50e4ed7a554987921d55fc" };
