77565b430c58d15b26585638dcbb166fdf390287
myzel394 <miguel.kra@gmail.com>
2021-06-01 13:45:10 +0200

    added icons to note_actions.js

--- src/public/app/widgets/note_actions.js
+++ src/public/app/widgets/note_actions.js
@@ -57,6 +57,30 @@ const TPL = `
     .slider.checked:before {
         transform: translateX(26px);
     }
+    
+    .dropdown-center {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    
+    .dropdown-icon {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    
+    .dropdown-icon > div {
+        margin-right: .2em;
+    }
+    
+    .switch-handler .switch {
+        margin-bottom: 0; /* Centers switch */
+    }
+    
+    .inset {
+        padding-left: 3em;
+    }
     </style>
 
     <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn btn-sm dropdown-toggle">
@@ -65,31 +89,80 @@ const TPL = `
     </button>
     <div class="dropdown-menu dropdown-menu-right">
         <a data-trigger-command="renderActiveNote" class="dropdown-item render-note-button"><kbd data-command="renderActiveNote"></kbd> Re-render note</a>
-        <div class="dropdown-item protect-button">
-            Protect the note
+        <div class="dropdown-item dropdown-center protect-button switch-handler">
+            <div class="dropdown-icon">
+                <div class="bx bx-lock"></div>
+                Protect the note
+            </div>
         
             <span title="Note is not protected, click to make it protected">
                 <label class="switch">
                 <span class="slider"></span>
             </span>
         </div>
-        <div class="dropdown-item unprotect-button">
-            Unprotect the note
+        <div class="dropdown-item dropdown-center unprotect-button switch-handler">
+            <div class="dropdown-icon">
+                <div class="bx bx-lock-open"></div>
+                Unprotect the note
+            </div>
         
             <span title="Note is protected, click to make it unprotected">
                 <label class="switch">
                 <span class="slider checked"></span>
             </span>
         </div>
-        <a data-trigger-command="findInText" class="dropdown-item">Search in note <kbd data-command="findInText"></a>
-        <a data-trigger-command="showNoteRevisions" class="dropdown-item show-note-revisions-button">Revisions</a>
-        <a data-trigger-command="showLinkMap" class="dropdown-item show-link-map-button"><kbd data-command="showLinkMap"></kbd> Link map</a>
-        <a data-trigger-command="showNoteSource" class="dropdown-item show-source-button"><kbd data-command="showNoteSource"></kbd> Note source</a>
-        <a data-trigger-command="openNoteExternally" class="dropdown-item open-note-externally-button"><kbd data-command="openNoteExternally"></kbd> Open note externally</a>
-        <a class="dropdown-item import-files-button">Import files</a>
-        <a class="dropdown-item export-note-button">Export note</a>
-        <a data-trigger-command="printActiveNote" class="dropdown-item print-note-button"><kbd data-command="printActiveNote"></kbd> Print note</a>
-        <a data-trigger-command="showNoteInfo" class="dropdown-item show-note-info-button"><kbd data-command="showNoteInfo"></kbd> Note info</a>
+        <a data-trigger-command="findInText" class="dropdown-item">
+            <div class="bx bx-search"></div>
+            Search in note
+            <kbd data-command="findInText">
+        </a>
+        <a data-trigger-command="showNoteRevisions" class="dropdown-item show-note-revisions-button">
+            <div class="dropdown-icon">
+                <div class="bx bx-list-ul"></div>
+                Revisions
+            </div>
+        </a>
+        <a data-trigger-command="showLinkMap" class="dropdown-item show-link-map-button inset">
+            <kbd data-command="showLinkMap"></kbd>
+            Link map
+        </a>
+        <a data-trigger-command="showNoteSource" class="dropdown-item show-source-button inset">
+            <kbd data-command="showNoteSource"></kbd>
+            Note source
+        </a>
+        <a data-trigger-command="openNoteExternally" class="dropdown-item open-note-externally-button inset">
+            <div class="dropdown-icon">
+                <div class="bx bx-link-external"></div>
+                Open note externally
+            </div>
+            <kbd data-command="openNoteExternally"></kbd>
+        </a>
+        <a class="dropdown-item import-files-button">
+            <div class="dropdown-icon">
+                <div class="bx bxs-file-import"></div>
+                Import files
+            </div>
+        </a>
+        <a class="dropdown-item export-note-button">
+            <div class="dropdown-icon">
+                <div class="bx bx-export"></div>
+                Export note
+            </div>
+        </a>
+        <a data-trigger-command="printActiveNote" class="dropdown-item print-note-button inset">
+            <div class="dropdown-icon">
+                <div class="bx bx-printer"></div>
+                Print note
+            </div>
+            <kbd data-command="printActiveNote"></kbd>
+        </a>
+        <a data-trigger-command="showNoteInfo" class="dropdown-item show-note-info-button inset">
+            <div class="dropdown-icon">
+                <div class="bx bx-info-circle"></div>
+                Note info
+            </div>
+            <kbd data-command="showNoteInfo"></kbd>
+        </a>
     </div>
 </div>`;
 
