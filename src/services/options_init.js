b85cf07a28bfc1c33bc91360e6203907ee81685a
Abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-09-20 21:08:41 +0200

    Add config option to change automatic readonly size (#2174)

--- src/services/options_init.js
+++ src/services/options_init.js
@@ -87,6 +87,8 @@ const defaultOptions = [
     { name: 'debugModeEnabled', value: 'false', isSynced: false },
     { name: 'headingStyle', value: 'underline', isSynced: true },
     { name: 'autoCollapseNoteTree', value: 'true', isSynced: true },
+    { name: 'autoReadonlySize', value: '10000', isSynced: false },
+    { name: 'autoCodeReadonlySize', value: '30000', isSynced: false },
 ];
 
 function initStartupOptions() {
