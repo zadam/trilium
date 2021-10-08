11e9f8403edd257ae0fdd646d40842a42f8eec90
zadam <zadam.apps@gmail.com>
2021-10-08 22:32:59 +0200

    release 0.48.0-beta

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2021-10-04T22:18:33+02:00", buildRevision: "1f75b9c92f1f695e4f43230f88d676ab8a0b39c0" };
+module.exports = { buildDate:"2021-10-08T22:32:59+02:00", buildRevision: "ba676587df01c494bcd1373b90254b570dd0b7aa" };
