4c7c53d8c8ef21c3ce0a883b5a34c75b507bb32c
zadam <zadam.apps@gmail.com>
2021-11-20 12:49:12 +0100

    retry for OpenNoteButtonWidget

--- src/public/app/widgets/buttons/open_note_button_widget.js
+++ src/public/app/widgets/buttons/open_note_button_widget.js
@@ -5,6 +5,18 @@ import froca from "../../services/froca.js";
 export default class OpenNoteButtonWidget extends ButtonWidget {
     targetNote(noteId) {
         froca.getNote(noteId).then(note => {
+            if (!note) {
+                console.log(`Note ${noteId} has not been found. This might happen on the first run before the target note is created.`);
+
+                if (!this.retried) {
+                    this.retried = true;
+
+                    setTimeout(() => this.targetNote(noteId), 15000); // should be higher than timeout for createMissingSpecialNotes
+                }
+
+                return;
+            }
+
             this.icon(note.getIcon());
             this.title(note.title);
 
