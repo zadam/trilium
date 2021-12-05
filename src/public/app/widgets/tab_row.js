2f5f11634582ae4abf3c543659672ab8ff83c6ac
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2021-12-05 12:07:53 +0000

    Improve tabs background coloring (#2395)
    * improved tabs background coloring
    
    * improvements
    
    * reverted changes
    
    * color improvements
    
    * added light theme

--- src/public/app/widgets/tab_row.js
+++ src/public/app/widgets/tab_row.js
@@ -187,6 +187,14 @@ const TAB_ROW_TPL = `
         cursor: pointer;
     }
     
+    .tab-row-widget .note-tab:hover .note-tab-wrapper {
+        background-color: var(--inactive-tab-hover-background-color);
+    }
+    
+    .tab-row-widget .note-tab[active]:hover .note-tab-wrapper {
+        background-color: var(--active-tab-hover-background-color);
+    }
+    
     .tab-row-widget .note-tab .note-tab-close:hover {
         background-color: var(--hover-item-background-color);
         color: var(--hover-item-text-color);
