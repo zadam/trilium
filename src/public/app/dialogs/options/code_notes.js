7d73a3542b2a67ec6f82d8cbe4df9cb896c42245
DHMike57 <14224881+DHMike57@users.noreply.github.com>
2021-12-26 11:23:05 +0000

    Add option for vim keymap in codemirror

--- src/public/app/dialogs/options/code_notes.js
+++ src/public/app/dialogs/options/code_notes.js
@@ -1,7 +1,15 @@
 import mimeTypesService from "../../services/mime_types.js";
 import options from "../../services/options.js";
+import server from "../../services/server.js";
+import toastService from "../../services/toast.js";
+import utils from "../../services/utils.js";
 
 const TPL = `
+<h4>Use vim keybindings in CodeNotes (no ex mode)</h4>
+<div class="custom-control custom-checkbox">
+    <input type="checkbox" class="custom-control-input" id="vim-keymap-enabled">
+    <label class="custom-control-label" for="vim-keymap-enabled">Enable Vim Keybindings</label>
+</div>
 <h4>Available MIME types in the dropdown</h4>
 
 <ul id="options-mime-types" style="max-height: 500px; overflow: auto; list-style-type: none;"></ul>`;
@@ -10,12 +18,18 @@ export default class CodeNotesOptions {
     constructor() {
         $("#options-code-notes").html(TPL);
 
+        this.$vimKeymapEnabled = $("#vim-keymap-enabled");
+        this.$vimKeymapEnabled.on('change', () => {
+            const opts = { 'vimKeymapEnabled': this.$vimKeymapEnabled.is(":checked") ? "true" : "false" };
+            server.put('options', opts).then(() => toastService.showMessage("Options change have been saved."));
+            return false;
+        });
         this.$mimeTypes = $("#options-mime-types");
     }
 
-    async optionsLoaded() {
+    async optionsLoaded(options) {
         this.$mimeTypes.empty();
-
+        this.$vimKeymapEnabled.prop("checked", options['vimKeymapEnabled'] === 'true');
         let idCtr = 1;
 
         for (const mimeType of await mimeTypesService.getMimeTypes()) {
