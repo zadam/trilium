c421ee79b0df62f452a8802e0ea9ae9a83005dde
zadam <zadam.apps@gmail.com>
2022-05-10 23:47:25 +0200

    increase DB version

--- src/services/app_info.js
+++ src/services/app_info.js
@@ -4,7 +4,7 @@ const build = require('./build');
 const packageJson = require('../../package');
 const {TRILIUM_DATA_DIR} = require('./data_dir');
 
-const APP_DB_VERSION = 194;
+const APP_DB_VERSION = 195;
 const SYNC_VERSION = 25;
 const CLIPPER_PROTOCOL_VERSION = "1.0";
 
