433003cf384ea7399af23e3da2859748fecc570f
zadam <zadam.apps@gmail.com>
2022-06-05 22:50:02 +0200

    fix migration

--- db/migrations/0196__rename_bulk_actions.sql
+++ db/migrations/0196__rename_bulk_actions.sql
@@ -1,2 +1,2 @@
-UPDATE attributes SET name = 'updateLabelValue' WHERE name = 'setLabelValue' AND type = 'label';
-UPDATE attributes SET name = 'updateRelationTarget' WHERE name = 'setRelationTarget' AND type = 'label';
+UPDATE attributes SET value = replace(value, 'setLabelValue', 'updateLabelValue') WHERE name = 'action' AND type = 'label';
+UPDATE attributes SET value = replace(value, 'setRelationTarget', 'updateRelationTarget') WHERE name = 'action' AND type = 'label';
