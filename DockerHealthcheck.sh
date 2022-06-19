8ac776f305ddfd8e1a4d53e7d3742345cbffc151
Bill Sideris <bill88t@feline.gr>
2022-06-19 12:39:20 +0300

    Slowly making it an oneliner, added line check

--- DockerHealthcheck.sh
+++ DockerHealthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if wget --spider -S "127.0.0.1:8080/login" 2>&1 | grep -w "HTTP/1.1 200 OK" ; then
+if wget --spider -S "127.0.0.1:8080/login" 2>&1 | awk 'NR==2' | grep -w "HTTP/1.1 200 OK" ; then
     exit 0
 else
     exit 1
