7c90f1a56dcf53e4b35cb8307263921ee0668fb8
zadam <zadam.apps@gmail.com>
2022-07-30 23:29:30 +0200

    increasing DB and sync version because of encryption IV change

--- src/services/app_info.js
+++ src/services/app_info.js
@@ -4,8 +4,8 @@ const build = require('./build');
 const packageJson = require('../../package');
 const {TRILIUM_DATA_DIR} = require('./data_dir');
 
-const APP_DB_VERSION = 196;
-const SYNC_VERSION = 25;
+const APP_DB_VERSION = 197;
+const SYNC_VERSION = 26;
 const CLIPPER_PROTOCOL_VERSION = "1.0";
 
 module.exports = {
