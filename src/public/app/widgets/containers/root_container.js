8acd3851b0a5ef40fb33b8a345efcc63e429e39e
zadam <zadam.apps@gmail.com>
2022-08-02 17:33:52 +0200

    cssClass label cannot be applied on the root widget because of note splits, fixes #3032

--- src/public/app/widgets/containers/root_container.js
+++ src/public/app/widgets/containers/root_container.js
@@ -1,6 +1,4 @@
 import FlexContainer from "./flex_container.js";
-import utils from "../../services/utils.js";
-import appContext from "../../services/app_context.js";
 
 export default class RootContainer extends FlexContainer {
     constructor() {
@@ -8,39 +6,5 @@ export default class RootContainer extends FlexContainer {
 
         this.id('root-widget');
         this.css('height', '100%');
-    }
-
-    refresh() {
-        this.$widget.removeClass(); // remove all classes
-        const note = appContext.tabManager.getActiveContextNote();
-
-        if (note) {
-            this.$widget.addClass(note.getCssClass());
-
-            this.$widget.addClass(utils.getNoteTypeClass(note.type));
-            this.$widget.addClass(utils.getMimeTypeClass(note.mime));
-
-            this.$widget.toggleClass("protected", note.isProtected);
-        }
-    }
-
-    noteSwitchedEvent() {
-        this.refresh();
-    }
-
-    activeContextChangedEvent() {
-        this.refresh();
-    }
-
-    noteSwitchedAndActivatedEvent() {
-        this.refresh();
-    }
-
-    entitiesReloadedEvent({loadResults}) {
-        const note = appContext.tabManager.getActiveContextNote();
-        
-        if (note && loadResults.isNoteReloaded(note.noteId)) {
-            this.refresh();
-        }
     }
 }
