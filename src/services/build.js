d33f2a9f1c94380ff8e62e91cab002209ae0ed6f
zadam <zadam.apps@gmail.com>
2022-08-07 23:16:17 +0200

    release 0.54.2

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2022-08-02T21:56:34+02:00", buildRevision: "12af3d05f04b9406af55ac301c0069eb385957c8" };
+module.exports = { buildDate:"2022-08-07T23:16:17+02:00", buildRevision: "b6f16112183d2b62db66de89c16def1e072b3b9a" };
