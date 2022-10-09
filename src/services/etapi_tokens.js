c2308b7a5d93a136cba95088655701ba4537be56
zadam <zadam.apps@gmail.com>
2022-10-09 21:34:01 +0200

    fix comment

--- src/services/etapi_tokens.js
+++ src/services/etapi_tokens.js
@@ -32,7 +32,7 @@ function parseAuthToken(auth) {
 
     if (auth.startsWith("Basic ")) {
         // allow also basic auth format for systems which allow this type of authentication
-        // expect ETAPI token in the password field, ignore username
+        // expect ETAPI token in the password field, require "etapi" username
         // https://github.com/zadam/trilium/issues/3181
         const basicAuthStr = utils.fromBase64(auth.substring(6)).toString("UTF-8");
         const basicAuthChunks = basicAuthStr.split(":");
