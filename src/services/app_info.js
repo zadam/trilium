eaab82551fb8d138786ab61f8d56710e787cbe81
zadam <zadam.apps@gmail.com>
2023-02-17 23:23:53 +0100

    API migration to remove deprecated calls

--- src/services/app_info.js
+++ src/services/app_info.js
@@ -4,7 +4,7 @@ const build = require('./build');
 const packageJson = require('../../package');
 const {TRILIUM_DATA_DIR} = require('./data_dir');
 
-const APP_DB_VERSION = 212;
+const APP_DB_VERSION = 213;
 const SYNC_VERSION = 29;
 const CLIPPER_PROTOCOL_VERSION = "1.0";
 
