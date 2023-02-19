fc67dd4d0ac865f2220cbfe9fa6e15ae24ddabc7
zadam <zadam.apps@gmail.com>
2023-02-19 21:36:05 +0100

    release 0.59.0-beta

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"2023-02-13T21:50:54+01:00", buildRevision: "17085e5578d2a20a77a6ade058f74e6d5b798ecc" };
+module.exports = { buildDate:"2023-02-19T21:36:05+01:00", buildRevision: "232dad66d10da0588344228ec877dfea999218fe" };
