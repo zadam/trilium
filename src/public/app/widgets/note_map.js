6612a3a550a2c870941b0ba6d2f3f72d334c6a0e
soulsands <407221377@qq.com>
2023-04-06 20:59:09 +0800

    fix: correct note-map resizing

--- src/public/app/widgets/note_map.js
+++ src/public/app/widgets/note_map.js
@@ -55,7 +55,14 @@ export default class NoteMapWidget extends NoteContextAwareWidget {
         this.$container = this.$widget.find(".note-map-container");
         this.$styleResolver = this.$widget.find('.style-resolver');
 
-        window.addEventListener('resize', () => this.setDimensions(), false);
+
+        try {
+            new ResizeObserver(() => this.setDimensions()).observe(this.$container[0])
+        } catch (error) {
+            window.addEventListener('resize', () => this.setDimensions(), false);
+        }
+
+
 
         this.$widget.find(".map-type-switcher button").on("click",  async e => {
             const type = $(e.target).closest("button").attr("data-type");
