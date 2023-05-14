8e25563b11e50c5f5520d67333373d7bddf36698
SiriusXT <1160925501@qq.com>
2023-05-14 17:24:54 +0800

    Add button: show toc

--- src/public/app/widgets/toc.js
+++ src/public/app/widgets/toc.js
@@ -262,11 +262,12 @@ export default class TocWidget extends RightPanelWidget {
         $noteSplit = $noteContainer.closest('.component.note-split.type-text.mime-text-html');
         $noteSplit.find(".ribbon-button-container").append(`<button type="button" class="icon-action bx bx-menu-alt-right show-toc" title="Show Toc"></button>`);
         var thisClick = this;
-        $noteSplit.find(".icon-action.bx.bx-menu-alt-right.show-toc").on("click", async function () {
+        var showTocButton = $noteSplit.find(".icon-action.bx.bx-menu-alt-right.show-toc");
+        showTocButton.on("click", async function () {
             thisClick.noteContext.viewScope.tocTemporarilyHidden = false;
             await thisClick.refresh();
             thisClick.triggerCommand('reEvaluateRightPaneVisibility');
-            $noteSplit.find(".icon-action.bx.bx-menu-alt-right.show-toc").remove()
+            showTocButton.remove()
         })
     }
 
