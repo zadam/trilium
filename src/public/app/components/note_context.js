d26a0fae175d1abf6d24fdab5ca03d4c257f57ae
zadam <zadam.apps@gmail.com>
2023-05-31 00:03:20 +0200

    if a note context has sub contexts, then it has to be saved even if empty, fixes #3985

--- src/public/app/components/note_context.js
+++ src/public/app/components/note_context.js
@@ -176,9 +176,12 @@ class NoteContext extends Component {
     }
 
     getTabState() {
-        if (!this.notePath && this.hoistedNoteId === 'root') {
+        if (this.hoistedNoteId !== 'root') {
             // keeping empty hoisted tab is esp. important for mobile (e.g. opened launcher config)
-            return null;
+
+            if (!this.notePath && this.getSubContexts().length === 0) {
+                return null;
+            }
         }
 
         return {
