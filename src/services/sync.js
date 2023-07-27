5b0f487f3fd12abafea3c0f0d76e6311e5eb153e
zadam <zadam.apps@gmail.com>
2023-07-27 14:50:18 +0200

    handle ERR_* errors gracefully

--- src/services/sync.js
+++ src/services/sync.js
@@ -58,9 +58,7 @@ async function sync() {
 
         if (e.message &&
                 (e.message.includes('ECONNREFUSED') ||
-                 e.message.includes('ERR_CONNECTION_REFUSED') ||
-                 e.message.includes('ERR_ADDRESS_UNREACHABLE') ||
-                 e.message.includes('ERR_CONNECTION_RESET') ||
+                 e.message.includes('ERR_') || // node network errors
                  e.message.includes('Bad Gateway'))) {
 
             ws.syncFailed();
