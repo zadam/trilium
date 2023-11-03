1ebdb0f5e1a5cc1b7f8c36af5e1f750141ab062b
zadam <zadam.apps@gmail.com>
2023-11-04 00:10:54 +0100

    emergency disabling of image compression since it appears to make problems in migration to 0.61

--- db/migrations/0220__migrate_images_to_attachments.js
+++ db/migrations/0220__migrate_images_to_attachments.js
@@ -3,8 +3,12 @@ module.exports = () => {
     const becca = require("../../src/becca/becca");
     const cls = require("../../src/services/cls");
     const log = require("../../src/services/log");
+    const sql = require("../../src/services/sql");
 
     cls.init(() => {
+        // emergency disabling of image compression since it appears to make problems in migration to 0.61
+        sql.execute(`UPDATE options SET value = 'false' WHERE name = 'compressImages'`);
+
         beccaLoader.load();
 
         for (const note of Object.values(becca.notes)) {
