e9282976d5d1b28d7c2dd028a1ff8164125c71f5
Elian Doran <contact@eliandoran.me>
2023-11-08 23:53:08 +0200

    Fix application icon

--- forge.config.js
+++ forge.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   packagerConfig: {
     asar: true,
+    // icon will break once we add .dmg support, since the .ico & .icns have to be in same dir (see https://www.electronforge.io/guides/create-and-add-icons#windows-and-macos)
+    icon: "./images/app-icons/win/icon"
   },
   rebuildConfig: {},
   makers: [
@@ -8,9 +10,8 @@ module.exports = {
       name: '@electron-forge/maker-squirrel',
       config: {
         setupIcon: "./images/app-icons/win/icon.ico",
-        icon: "./images/app-icons/win/icon.ico",
         loadingGif: "./images/app-icons/win/setup-banner.gif"
-      },
+      }
     },
     {
       name: '@electron-forge/maker-zip',
