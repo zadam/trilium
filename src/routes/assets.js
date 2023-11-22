a439be79b3d31e3a780fe5486dbd38b77f020dc3
zadam <zadam.apps@gmail.com>
2023-11-22 20:11:41 +0100

    load jquery from node_modules

--- src/routes/assets.js
+++ src/routes/assets.js
@@ -56,6 +56,8 @@ function register(app) {
     app.use(`/${assetPath}/node_modules/boxicons/fonts/`, persistentCacheStatic(path.join(srcRoot, '..', 'node_modules/boxicons/fonts/')));
 
     app.use(`/${assetPath}/node_modules/mermaid/dist/`, persistentCacheStatic(path.join(srcRoot, '..', 'node_modules/mermaid/dist/')));
+
+    app.use(`/${assetPath}/node_modules/jquery/dist/`, persistentCacheStatic(path.join(srcRoot, '..', 'node_modules/jquery/dist/')));
 }
 
 module.exports = {
