8acb64198cb4bccc4e43dea649ae323d4f3a6928
Elian Doran <contact@eliandoran.me>
2024-02-18 01:24:57 +0200

    server-ts: Port services/search/services/build_comparator

--- src/services/search/services/build_comparator.ts
+++ src/services/search/services/build_comparator.ts
@@ -1,6 +1,6 @@
-const cachedRegexes = {};
+const cachedRegexes: Record<string, RegExp> = {};
 
-function getRegex(str) {
+function getRegex(str: string) {
     if (!(str in cachedRegexes)) {
         cachedRegexes[str] = new RegExp(str);
     }
@@ -8,31 +8,36 @@ function getRegex(str) {
     return cachedRegexes[str];
 }
 
-const stringComparators = {
+type Comparator<T> = (comparedValue: T) => ((val: string) => boolean);
+
+const stringComparators: Record<string, Comparator<string>> = {
     "=": comparedValue => (val => val === comparedValue),
     "!=": comparedValue => (val => val !== comparedValue),
     ">": comparedValue => (val => val > comparedValue),
     ">=": comparedValue => (val => val >= comparedValue),
     "<": comparedValue => (val => val < comparedValue),
     "<=": comparedValue => (val => val <= comparedValue),
-    "*=": comparedValue => (val => val && val.endsWith(comparedValue)),
-    "=*": comparedValue => (val => val && val.startsWith(comparedValue)),
-    "*=*": comparedValue => (val => val && val.includes(comparedValue)),
-    "%=": comparedValue => (val => val && !!getRegex(comparedValue).test(val)),
+    "*=": comparedValue => (val => !!val && val.endsWith(comparedValue)),
+    "=*": comparedValue => (val => !!val && val.startsWith(comparedValue)),
+    "*=*": comparedValue => (val => !!val && val.includes(comparedValue)),
+    "%=": comparedValue => (val => !!val && !!getRegex(comparedValue).test(val)),
 };
 
-const numericComparators = {
+const numericComparators: Record<string, Comparator<number>> = {
     ">": comparedValue => (val => parseFloat(val) > comparedValue),
     ">=": comparedValue => (val => parseFloat(val) >= comparedValue),
     "<": comparedValue => (val => parseFloat(val) < comparedValue),
     "<=": comparedValue => (val => parseFloat(val) <= comparedValue)
 };
 
-function buildComparator(operator, comparedValue) {
+function buildComparator(operator: string, comparedValue: string) {
     comparedValue = comparedValue.toLowerCase();
 
-    if (operator in numericComparators && !isNaN(comparedValue)) {
-        return numericComparators[operator](parseFloat(comparedValue));
+    if (operator in numericComparators) {
+        const floatValue = parseFloat(comparedValue);
+        if (!isNaN(floatValue)) {
+            return numericComparators[operator](floatValue);
+        }
     }
 
     if (operator in stringComparators) {
@@ -40,4 +45,4 @@ function buildComparator(operator, comparedValue) {
     }
 }
 
-module.exports = buildComparator;
+export = buildComparator;
