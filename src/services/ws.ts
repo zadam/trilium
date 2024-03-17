9f6a8dc75c18ad079a2a65b16f6a080aace10ad7
Elian Doran <contact@eliandoran.me>
2024-03-17 21:34:50 +0200

    server-ts: Fix undefined in ws

--- src/services/ws.ts
+++ src/services/ws.ts
@@ -29,10 +29,10 @@ let lastSyncedPush: number | null = null;
 interface Message {
     type: string;
     data?: {
-        lastSyncedPush?: number,
+        lastSyncedPush?: number | null,
         entityChanges?: any[]
     },
-    lastSyncedPush?: number,
+    lastSyncedPush?: number | null,
     
     progressCount?: number;
     taskId?: string;
@@ -222,7 +222,7 @@ function sendPing(client: WebSocket, entityChangeIds = []) {
     sendMessage(client, {
         type: 'frontend-update',
         data: {
-            lastSyncedPush: lastSyncedPush || undefined,
+            lastSyncedPush,
             entityChanges
         }
     });
@@ -237,19 +237,19 @@ function sendTransactionEntityChangesToAllClients() {
 }
 
 function syncPullInProgress() {
-    sendMessageToAllClients({ type: 'sync-pull-in-progress', lastSyncedPush: lastSyncedPush || undefined });
+    sendMessageToAllClients({ type: 'sync-pull-in-progress', lastSyncedPush });
 }
 
 function syncPushInProgress() {
-    sendMessageToAllClients({ type: 'sync-push-in-progress', lastSyncedPush: lastSyncedPush || undefined });
+    sendMessageToAllClients({ type: 'sync-push-in-progress', lastSyncedPush });
 }
 
 function syncFinished() {
-    sendMessageToAllClients({ type: 'sync-finished', lastSyncedPush: lastSyncedPush || undefined });
+    sendMessageToAllClients({ type: 'sync-finished', lastSyncedPush });
 }
 
 function syncFailed() {
-    sendMessageToAllClients({ type: 'sync-failed', lastSyncedPush: lastSyncedPush || undefined });
+    sendMessageToAllClients({ type: 'sync-failed', lastSyncedPush });
 }
 
 function reloadFrontend(reason: string) {
