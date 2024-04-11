4638351ec8eba61b387141047c3c9d4a483a3072
Elian Doran <contact@eliandoran.me>
2024-04-11 22:21:38 +0300

    server-ts: Update progress script

--- _check_ts_progress.sh
+++ _check_ts_progress.sh
@@ -4,7 +4,7 @@ cloc HEAD \
     --git --md \
     --include-lang=javascript,typescript \
     --found=filelist.txt \
-    --exclude-dir=public,libraries
+    --exclude-dir=public,libraries,views,docs
 
 grep -R \.js$ filelist.txt
 rm filelist.txt
